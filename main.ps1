dbd1807449dd2854ba34139264c13e37cef6b8d4
MeatyPetey-5000 <113252283+MeatyPetey-5000@users.noreply.github.com>
2025-05-17 01:57:05 +0100

    Update main.ps1
    good luck

--- main.ps1
+++ main.ps1
@@ -2,7 +2,7 @@
 # why is this done like this? are you stupid?
 # this sucks but i can't be bothered to do it properly, i'm just going to call it "legacy"
 # fix me
-# hacky hack that requires hacking. pls msg me for explanation.
+# hacky hack that requires hacking. pls msg me for explanation. If my employment has ended, you're shit out of luck. re-write this function. good luck.
 # michalesoft spaghetti
 # just add more memory for this function
 # you think this does nothing, BUT IT DOES DO SOMETHING! I DON'T KNOW WHAT! pls don't delete this comment or it will break
