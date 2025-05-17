fa796f601627d1fea8dd26805da1cc392e043f49
MeatyPetey-5000 <113252283+MeatyPetey-5000@users.noreply.github.com>
2025-05-17 01:27:32 +0100

    Update main.ps1
    mmmmmmm

--- main.ps1
+++ main.ps1
@@ -2,3 +2,4 @@
 # why is this done like this? are you stupid?
 # this sucks but i can't be bothered to do it properly, i'm just going to call it "legacy"
 # fix me
+# hacky hack that requires hacking. pls msg me for explanation.
